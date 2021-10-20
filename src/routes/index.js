b035bb438689ca0025806f9e0e55fa036459250c
DerekMalone <laxmalone@gmail.com>
2021-10-19 21:41:27 -0500

    saving in attempt to resolve issue

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,33 @@
-// index for router
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Team from '../views/Team';
+import NewPlayer from '../views/NewPlayer';
+
+export default function Routes({
+  player, setPlayer, team, setTeam,
+}) {
+  return (
+    <div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={() => <Team team={team} setTeam={setTeam} />}
+        />
+        <Route
+          exact
+          path="/player"
+          component={() => <NewPlayer player={player} setPlayer={setPlayer} />}
+        />
+      </Switch>
+    </div>
+  );
+}
+
+Routes.propTypes = {
+  player: PropTypes.arrayOf(PropTypes.object).isRequired,
+  team: PropTypes.arrayOf(PropTypes.object).isRequired,
+  setPlayer: PropTypes.func.isRequired,
+  setTeam: PropTypes.func.isRequired,
+};
---
+++ src/views/NewPlayer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NewPlayer() {
+  return <div>New Player</div>;
+}
